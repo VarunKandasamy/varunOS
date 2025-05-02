8eaa4869cfd44b2c793351a62b0dc5f50e6ca11d
VarunKandasamy <varuntheinventor@gmail.com>
2025-05-02 12:21:56 -0700

    Bootloader experiments w/ Stack

--- Makefile
+++
@@ -1,4 +0,0 @@
-SRC_DIR = src
-BUILD_DIR = build
-
-$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
--- src/main.asm
+++ src/main.asm
@@ -9,11 +9,29 @@ mov ah,0x0e
 mov al,'I'
 int 0x10
 
+;experimenting with stack pointer
+;reset stack, push characters, then printg
+mov bp, 0x9000
+mov sp, bp
+push 'E'
+push 'Y'
+push 'B'
+
+pop ax
+mov ah, 0x0e
+int 0x10
+pop ax
+mov ah, 0x0e
+int 0x10
+pop ax
+mov ah, 0x0e
+int 0x10
+
 main:
 	hlt
 
 halt:
-	jmp halt
+	jmp $
 
 times 510 - ($-$$) db 0
 dw 0xAA55
